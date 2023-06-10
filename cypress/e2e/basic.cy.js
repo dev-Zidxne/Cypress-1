643b92405cdac14fac197d6fcf0c944ce2ce175b
Zidane Innis <zidaneinnis@gmail.com>
2023-06-10 01:17:58 -0500

    Implicit and Explicit Assertions

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -8,7 +8,9 @@ describe('tasks page', () => {
 
 	it('should display the page title', () => {
 		cy.visit('http://localhost:5173');
-		cy.contains('h1', 'My Cypress Course Tasks');
+		cy.get('h1').should('have.length', 1);
+		cy.get('h1').contains('My Cypress Course Tasks');
+		// cy.contains('h1').should('have.length', 1);
 		// cy.contains('My Cypress Course Tasks');
 	});
 });
--- src/App.jsx
+++ src/App.jsx
@@ -7,59 +7,59 @@ import TaskControl from './components/TaskControl';
 import TaskList from './components/TaskList';
 
 function App() {
-  const [isAddingTask, setIsAddingTask] = useState(false);
-  const [tasks, setTasks] = useState([]);
-  const [appliedFilter, setAppliedFilter] = useState('all');
+	const [isAddingTask, setIsAddingTask] = useState(false);
+	const [tasks, setTasks] = useState([]);
+	const [appliedFilter, setAppliedFilter] = useState('all');
 
-  const displayedTasks = tasks.filter((task) => {
-    if (appliedFilter === 'all') {
-      return true;
-    }
-    return task.category === appliedFilter;
-  });
+	const displayedTasks = tasks.filter((task) => {
+		if (appliedFilter === 'all') {
+			return true;
+		}
+		return task.category === appliedFilter;
+	});
 
-  function startAddTaskHandler() {
-    setIsAddingTask(true);
-  }
+	function startAddTaskHandler() {
+		setIsAddingTask(true);
+	}
 
-  function cancelAddTaskHandler() {
-    setIsAddingTask(false);
-  }
+	function cancelAddTaskHandler() {
+		setIsAddingTask(false);
+	}
 
-  function addTaskHandler(taskData) {
-    setTasks((prevTasks) => {
-      return [
-        ...prevTasks,
-        {
-          id: Math.random().toString(),
-          ...taskData,
-        },
-      ];
-    });
-    setIsAddingTask(false);
-  }
+	function addTaskHandler(taskData) {
+		setTasks((prevTasks) => {
+			return [
+				...prevTasks,
+				{
+					id: Math.random().toString(),
+					...taskData,
+				},
+			];
+		});
+		setIsAddingTask(false);
+	}
 
-  function setFilterHandler(category) {
-    setAppliedFilter(category);
-  }
+	function setFilterHandler(category) {
+		setAppliedFilter(category);
+	}
 
-  return (
-    <>
-      {isAddingTask && (
-        <Modal onClose={cancelAddTaskHandler}>
-          <NewTask onAddTask={addTaskHandler} onCancel={cancelAddTaskHandler} />
-        </Modal>
-      )}
-      <Header />
-      <main>
-        <TaskControl
-          onStartAddTask={startAddTaskHandler}
-          onSetFilter={setFilterHandler}
-        />
-        <TaskList tasks={displayedTasks} />
-      </main>
-    </>
-  );
+	return (
+		<>
+			{isAddingTask && (
+				<Modal onClose={cancelAddTaskHandler}>
+					<NewTask onAddTask={addTaskHandler} onCancel={cancelAddTaskHandler} />
+				</Modal>
+			)}
+			<Header />
+			<main>
+				<TaskControl
+					onStartAddTask={startAddTaskHandler}
+					onSetFilter={setFilterHandler}
+				/>
+				<TaskList tasks={displayedTasks} />
+			</main>
+		</>
+	);
 }
 
 export default App;
