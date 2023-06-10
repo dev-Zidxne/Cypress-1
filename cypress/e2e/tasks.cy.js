fd174aac581c544051978fe4313a3809ac67b6c5
Zidane Innis <zidaneinnis@gmail.com>
2023-06-10 01:47:10 -0500

    Simulating User Interaction

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,9 @@
+/// <reference types='Cypress' />
+
+describe('tasks management', () => {
+	it('should open and close the new task modal', () => {
+		cy.visit('http://localhost:5173');
+		cy.contains('button', 'Add Task').click();
+		cy.get('.backdrop').click({ force: true });
+	});
+});
