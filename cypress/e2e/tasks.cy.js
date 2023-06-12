702d501d13125d611f7e232f61a226ed13c35b8a
Zidane Innis <zidaneinnis@gmail.com>
2023-06-12 15:07:07 -0500

    Simulating Keyboard Typing & Writing Good Assertions

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -5,7 +5,24 @@ describe('tasks management', () => {
 		cy.visit('http://localhost:5173');
 		cy.contains('button', 'Add Task').click();
 		cy.get('.backdrop').click({ force: true });
-		cy.get('.backdrop').shodul('not.exist').click({ force: true });
+		cy.get('.backdrop').should('not.exist');
 		cy.get('modal').should('not.exist');
+
+		cy.contains('button', 'Add Task').click();
+		cy.contains('button', 'Cancel').click();
+		cy.get('.backdrop').should('not.exist');
+		cy.get('modal').should('not.exist');
+	});
+	it('should create a new task', () => {
+		cy.visit('http://localhost:5173');
+		cy.contains('Add Task').click();
+		cy.get('#title').type('New Task');
+		cy.get('#summary').type('A description');
+		cy.get('.modal').contains('Add Task').click();
+		cy.get('.backdrop').should('not.exist');
+		cy.get('modal').should('not.exist');
+		cy.get('.task').should('have.length', 1);
+		cy.get('.task h2').contains('New Task');
+		cy.get('.task p').contains('A description');
 	});
 });
