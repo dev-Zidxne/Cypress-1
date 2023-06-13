0edf7432f8bb190eb24a6ee314c8c125d5b8f67c
Zidane Innis <zidaneinnis@gmail.com>
2023-06-13 18:27:42 -0500

    Checking For Partial Text

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -25,4 +25,11 @@ describe('tasks management', () => {
 		cy.get('.task h2').contains('New Task');
 		cy.get('.task p').contains('A description');
 	});
+
+	it('should validate user input', () => {
+		cy.visit('http://localhost:5173');
+		cy.contains('button', 'Add Task').click();
+		cy.get('.modal').contains('Add Task').click();
+		cy.contains('Please provide values');
+	});
 });
